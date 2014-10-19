4be33e7b8dbdf9b7c2087b769ef606912c2df789
Stacey Reiman <mspanish@zoho.com>
2014-10-19 12:18:38 -0800

    faster 2nd highest algo

--- algos.js
+++ algos.js
@@ -1,4 +1,4 @@
-// SECOND HIGHEST ALGORITHM (created from scratch)
+// #1 SECOND HIGHEST ALGORITHM (created from scratch)
 
 var e = 0; // highest number
 var f = 0; // second highest number
@@ -27,11 +27,69 @@ function secondHighest(arr) {
 
 }// end secondHighest()
 
-var newArray = [3,7,5,9,54,62,33,0,23,48,13,99,34,57,9,88,11]
-console.log(secondHighest(newArray));
+var newArray = [3,7,5,9,54,62,33,0,23,48,13,99,34,57,9,88,11,20]
+
+//#2 (faster?) SECOND HIGHEST ALGORITHM (created from scratch)
+
+var e = 0; // highest number
+var f = 0; // second highest number
+
+var calls = 0;
+
+function secondHighestSplitting(arr) {
+    calls++
+    var arrayOfArrays = [];
+    var array_length = arr.length;
+    var highest = [0,0];
+    var t=0, i=0;
+/* since we know our length is a power of 2, we split these into arrays of 2 numbers each, adding the higher numbers on the left */
+
+    while (array_length>t) {
+
+        var num1 = arr[t];
+        var num2 = arr[t+1];
+
+        if (num1 > num2) {
+            arrayOfArrays.push([num1, num2])
+            calls++
+            calls++
+        }
+        else {
+            arrayOfArrays.push([num2, num1])
+            calls++
+        };
+
+        var num3 = arrayOfArrays[i][0];
+        var num4 = arrayOfArrays[i][1];
+
+        if (num3>highest[0]) { 
+            highest[0] = num3
+            calls++
+        }
+        else if (num3>highest[1]) { 
+            highest[1] = num3
+            calls++
+        }        
+        if (num4>highest[1]) {             
+            highest[1] = num4
+            calls++
+        };               
+        i++
+        t+=2
+    }
+
+console.log('our array of Arrays is '+JSON.stringify(arrayOfArrays));
+console.log('our highest array is '+JSON.stringify(highest))
+
+  return highest[1]
+
+}// end secondHighest()
+
+console.log(secondHighestSplitting(newArray));
+console.log('calls is '+calls)
 
 
-// MERGE SORT ALGORITHM - created from online examples
+//#3 MERGE SORT ALGORITHM - created from online examples
 
 var mergeCount = 0;
 var mergeSortCount = 0;
